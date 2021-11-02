cb4de75b245fab1a2160508077bd28105152defa
caposcar1998 <oscarcontrerascap@gmail.com>
2021-11-01 18:56:13 -0600

    configuracion inical Se configura el proyecto a nivel global

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <BrowserRouter>
+          <Switch>
+            <Route path='/' exact component={Login} />
+            <Route path='/home' exact  component={Home} />
+          </Switch>
+        </BrowserRouter>
+    </header>
+  </div>
   );
 }
 
