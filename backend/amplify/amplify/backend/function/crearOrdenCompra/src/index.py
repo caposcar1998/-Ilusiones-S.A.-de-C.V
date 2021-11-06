b6f8569442212ac64c20f72102e8df59e19318dd
caposcar1998 <oscarcontrerascap@gmail.com>
2021-11-05 18:48:20 -0600

    Se generan ordenenes en local

--- backend/amplify/amplify/backend/function/crearOrdenCompra/src/index.py
+++ backend/amplify/amplify/backend/function/crearOrdenCompra/src/index.py
@@ -32,12 +32,38 @@ def obtener_archivo_s3(nombre_archivo):
 
     binary_data = res['Body'].read()
     wb = openpyxl.load_workbook(BytesIO(binary_data),data_only=True)
+    ref = wb.active
     sheet = wb.active
     for row in sheet.iter_rows():
-        print(row)
+        column = 1
+        titulo = ""
         for cell in row:
-            try:
-                print(cell.value)
-            except:
-                pass
+            if(column == 1):
+                print("Columna",sheet.cell(row=1, column=column).value)
+                print("_Id", cell.value)
+                titulo = cell.value
+                workbook = Workbook()
+                sheet = workbook.active
+                sheet["A1"] = f"Orden de compra de:{cell.value}"
+
+
+            elif(column == 2):
+                print("Columna",sheet.cell(row=1, column=column).value)
+                print("Titulo", cell.value)
+                sheet["A2"] = f"Cliente :{cell.value}"
+            else:
+
+
+                if((cell.value != None)):
+                    print("Celular",ref.cell(row=1, column=column).value)
+                    print("Numero de celulares", cell.value) 
+                    sheet.cell(column=5, row=sheet.max_row+1, value=str(ref.cell(row=1, column=column).value))
+                    sheet.cell(column=6, row=sheet.max_row+1, value=cell.value)             
+            column = column + 1   
+
+
+        print()
+        workbook.save(filename="archivos/"+titulo+".xlsx")
+
+
 
